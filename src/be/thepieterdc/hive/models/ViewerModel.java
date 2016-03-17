4075753c29070b04a9eec0be23367150748aeb6c
Pieter De Clercq <pieterdeclercq@outlook.com>
2016-03-17 14:17:00 +0100

    Added ViewerModel

---
+++ src/be/thepieterdc/hive/models/ViewerModel.java
@@ -0,0 +1,13 @@
+package be.thepieterdc.hive.models;
+
+import be.thepieterdc.hive.helpers.Model;
+
+/**
+ * Model for the Viewer part.
+ * <p>
+ * Created at 17/03/16 14:08
+ *
+ * @author <a href="mailto:pieterdeclercq@outlook.com">Pieter De Clercq</a>
+ */
+public class ViewerModel extends Model {
+}
