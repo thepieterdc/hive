53e4a98f9f953661e2878fdcf8da3d26cacafd1b
Pieter De Clercq <pieterdeclercq@outlook.com>
2016-03-19 11:24:59 +0100

    Added Unit

--- src/be/thepieterdc/hive/helpers/Unit.java
+++ src/be/thepieterdc/hive/helpers/Unit.java
@@ -1,5 +1,8 @@
 package be.thepieterdc.hive.helpers;
 
+import be.thepieterdc.hive.data.UnitType;
+import javafx.scene.paint.Color;
+
 /**
  * A playable unit.
  * <p>
@@ -8,5 +11,28 @@ package be.thepieterdc.hive.helpers;
  * @author <a href="mailto:pieterdeclercq@outlook.com">Pieter De Clercq</a>
  */
 public class Unit {
-	public
+	private final Color player;
+	private final int rank;
+	private final UnitType type;
+
+	/**
+	 * Unit constructor.
+	 * @param player the player color
+	 * @param type the unit type
+	 */
+	public Unit(Color player, UnitType type) {
+		this(player, type, 0);
+	}
+
+	/**
+	 * Unit constructor.
+	 * @param player the player color
+	 * @param type the unit type
+	 * @param rank the rank of the unit
+	 */
+	public Unit(Color player, UnitType type, int rank) {
+		this.player = player;
+		this.rank = rank;
+		this.type = type;
+	}
 }
