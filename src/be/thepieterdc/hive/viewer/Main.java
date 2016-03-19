c7ecd57f5a6344f9181cfd67ecc16f8bab25d463
Pieter De Clercq <pieterdeclercq@outlook.com>
2016-03-19 18:57:02 +0100

    Added UI to Main

--- src/be/thepieterdc/hive/viewer/Main.java
+++ src/be/thepieterdc/hive/viewer/Main.java
@@ -1,13 +1,20 @@
 package be.thepieterdc.hive.viewer;
 
+import be.thepieterdc.hive.components.MovesPane;
+import be.thepieterdc.hive.components.UnitHexagon;
+import be.thepieterdc.hive.components.UnitPane;
+import be.thepieterdc.hive.data.UnitType;
 import be.thepieterdc.hive.exceptions.MalformedMoveException;
 import be.thepieterdc.hive.helpers.Move;
 import be.thepieterdc.hive.helpers.messages.ErrorMessage;
 import be.thepieterdc.hive.models.ViewerModel;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Orientation;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -39,7 +46,6 @@ public class Main extends Application {
 				List<String> movesString = Files.readAllLines(Paths.get(parameters.get(0)));
 				List<Move> movesMove = new ArrayList<>();
 				for(String s : movesString) {
-					System.out.println(s);
 					movesMove.add(Move.fromRepresentation(s));
 				}
 				this.model = new ViewerModel(movesMove, Color.BLACK, Color.WHITE);
@@ -49,10 +55,36 @@ public class Main extends Application {
 				throw new Exception("Invalid move: "+e.move());
 			}
 
-			//Placeholder//
-			Pane rootPane = new Pane();
+			MovesPane movesPane = new MovesPane(this.model);
 
-			Scene scene = new Scene(rootPane);
+			UnitHexagon hexOne = new UnitHexagon(UnitType.BEATLE, Color.TURQUOISE);
+			hexOne.scale(10);
+			hexOne.setTranslateX(-200);
+			UnitHexagon hexTwo = new UnitHexagon(UnitType.LADYBUG, Color.MEDIUMAQUAMARINE);
+			hexTwo.scale(10);
+			UnitHexagon hexThree = new UnitHexagon(UnitType.QUEEN, Color.BLANCHEDALMOND);
+			hexThree.scale(10);
+			hexThree.setTranslateX(200);
+
+			Group hexagons = new Group();
+			hexagons.getChildren().addAll(hexOne, hexTwo, hexThree);
+
+			StackPane playPane = new StackPane(hexagons);
+
+			SplitPane mainPane = new SplitPane(movesPane, playPane);
+			mainPane.setDividerPositions(0.0);
+
+			//TILEPANE//
+			UnitPane bottomPane = new UnitPane(this.model);
+
+			SplitPane root = new SplitPane(mainPane, bottomPane);
+			root.setDividerPositions(100.0);
+			root.setOrientation(Orientation.VERTICAL);
+			root.setPrefSize(800, 500);
+
+			this.model.move(0);
+
+			Scene scene = new Scene(root);
 			stage.setScene(scene);
 			stage.setTitle("Hive Viewer");
 			stage.show();
