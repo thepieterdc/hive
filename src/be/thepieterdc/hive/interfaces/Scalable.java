4d06017fe01086b40bc4d454b5b0c2671033c44c
Pieter De Clercq <pieterdeclercq@outlook.com>
2016-03-26 22:27:00 +0100

    Documented interfaces #26

--- src/be/thepieterdc/hive/interfaces/Scalable.java
+++ src/be/thepieterdc/hive/interfaces/Scalable.java
@@ -8,10 +8,18 @@ package be.thepieterdc.hive.interfaces;
  * @author <a href="mailto:pieterdeclercq@outlook.com">Pieter De Clercq</a>
  */
 public interface Scalable {
-
+	/**
+	 * @return the height of the node
+	 */
 	double height();
 
+	/**
+	 * @param factor the factor to scale the node with
+	 */
 	void scale(double factor);
 
+	/**
+	 * @return the width of the node
+	 */
 	double width();
 }
