0d9c733418b1b78978e4e206998e3597b415ab63
Pieter De Clercq <pieterdeclercq@outlook.com>
2016-04-11 12:19:19 +0200

    Added Player class. #37

--- src/hive/modes/PlayMode.java
+++ src/hive/modes/PlayMode.java
@@ -1,7 +1,15 @@
 package hive.modes;
 
+import hive.components.HivePane;
 import hive.helpers.Mode;
+import hive.models.PlayModel;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.util.List;
 
@@ -15,7 +23,19 @@ import java.util.List;
 public final class PlayMode extends Mode {
 	@Override
 	public void start(Stage s, List<String> p) {
-		System.out.println("Startmode");
+		Button b = new Button("Lokale multiplayer");
+		b.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				//TODO ASK INPUT//
+				s.close();
+				Stage playStage = new Stage();
+				playStage.setScene(new Scene(new HivePane(new PlayModel("Pieter","Tobiah"))));
+			}
+		});
+		s.initStyle(StageStyle.UNDECORATED);
+		s.setScene(new Scene(new VBox(b), 200, 200));
+		s.show();
 	}
 
 	@Override
