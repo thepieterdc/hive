da55a3354bcc5d22073ef2a0d93ca4a25c0b8cf2
Pieter De Clercq <pieterdeclercq@outlook.com>
2016-04-20 21:46:39 +0200

    Added winnerproperty

--- src/hive/models/PlayModel.java
+++ src/hive/models/PlayModel.java
@@ -31,12 +31,14 @@ public final class PlayModel extends HiveModel {
 	private final Player player2;
 
 	private final SimpleObjectProperty<Unit> selectedUnitProperty;
+	private final SimpleObjectProperty<Player> winnerProperty;
 
 	public PlayModel(String p1, String p2) {
 		super(Collections.singletonList(new StartMove()));
 		this.player1 = new Player(Players.BLACK, p1);
 		this.player2 = new Player(Players.WHITE, p2);
 		this.selectedUnitProperty = new SimpleObjectProperty<>();
+		this.winnerProperty = new SimpleObjectProperty<>();
 
 		AtomicInteger i = new AtomicInteger();
 		Arrays.asList(this.player1, this.player2).forEach(p -> EnumSet.allOf(UnitType.class).forEach(u -> IntStream.range(0, u.capacity()).forEach(c -> units[i.getAndIncrement()] = new Unit(p, u, c + 1))));
@@ -70,6 +72,9 @@ public final class PlayModel extends HiveModel {
 
 		this.boardStates.put(this.totalMoves - 1, BoardState.calculate(this.boardState(), m));
 		this.move(this.totalMoves - 1);
+
+		//TODO VERBETEREN//
+
 	}
 
 	public Player player1() {
@@ -86,5 +91,9 @@ public final class PlayModel extends HiveModel {
 
 	public Player turn() {
 		return this.totalMoves % 2 == 0 ? this.player1 : this.player2;
+	}
+
+	public SimpleObjectProperty<Player> winnerProperty() {
+		return this.winnerProperty;
 	}
 }
