3123b53128350dd581231ea526ee9be5b6d43faa
Pieter De Clercq <pieterdeclercq@outlook.com>
2016-04-30 23:45:41 +0200

    Added Ant Movement

--- src/hive/helpers/pathfinding/PathFinder.java
+++ src/hive/helpers/pathfinding/PathFinder.java
@@ -3,12 +3,12 @@ package hive.helpers.pathfinding;
 import hive.helpers.BoardState;
 import hive.helpers.HexCoordinate;
 import hive.helpers.Move;
+import hive.helpers.Unit;
 import hive.helpers.statevalidators.ContinuousContactValidator;
 import hive.helpers.statevalidators.SingleSwarmValidator;
 import hive.interfaces.Validatable;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -28,7 +28,33 @@ public abstract class PathFinder {
 	}
 
 	protected static boolean canPassThrough(BoardState state, HexCoordinate start, HexCoordinate dest) {
-		return state.freeNeighbours(start).stream().filter(h -> dest.distanceTo(h) == 1).collect(Collectors.toSet()).size() == 2;
+		return state.freeNeighbours(start).stream().filter(h -> dest.distanceTo(h) == 1).collect(Collectors.toSet()).size() >= 1;
+	}
+
+	protected boolean pathFind(BoardState state, Unit u, HexCoordinate dest) {
+		return this.pathFindRecursion(state, u, dest, new ArrayList<>(state.freeHexagons().size()));
+	}
+
+	private boolean pathFindRecursion(BoardState state, Unit u, HexCoordinate dest, Collection<HexCoordinate> path) {
+		if(u.location().equals(dest)) {
+			return true;
+		}
+		HexCoordinate start = u.location();
+		for(HexCoordinate h : state.freeNeighbours(u.location())) {
+			if(!path.contains(h)) {
+				path.add(h);
+				u.location(h);
+				BoardState newState = BoardState.calculate(state, u, h);
+				if(this.validState(newState)) {
+					if (this.pathFindRecursion(newState, u, dest, path)) {
+						return true;
+					}
+				}
+				u.location(start);
+				path.remove(h);
+			}
+		}
+		return false;
 	}
 
 	public abstract boolean valid(BoardState state, Move m, HexCoordinate dest);
