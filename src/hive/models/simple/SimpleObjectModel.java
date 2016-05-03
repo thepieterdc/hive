0c84927685c9ea3cefd00024ea5e55520d079a84
Pieter De Clercq <pieterdeclercq@outlook.com>
2016-05-03 19:42:59 +0200

    Added SimpleObjectModel. #68.

--- src/hive/models/simple/SimpleObjectModel.java
+++ src/hive/models/simple/SimpleObjectModel.java
@@ -3,11 +3,22 @@ package hive.models.simple;
 import hive.helpers.Model;
 
 /**
- * Description here
+ * A model that only has one Observable value.
+ * Reason for not using a <i>SimpleObjectProperty</i>: Lacking a removeAllListeners method.
  * <p>
  * Created at 3/05/16 19:30
  *
  * @author <a href="mailto:pieterdeclercq@outlook.com">Pieter De Clercq</a>
  */
-public class SimpleObjectModel extends Model {
+public class SimpleObjectModel<T> extends Model {
+	private T o;
+
+	public T get() {
+		return this.o;
+	}
+
+	public void set(T t) {
+		this.o = t;
+		this.notifyListeners();
+	}
 }
--- src/hive/properties/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Redefining the standard JavaFX properties to my own needs.
- * <p>
- * Created at 3/05/16 19:27
- *
- * @author <a href="mailto:pieterdeclercq@outlook.com">Pieter De Clercq</a>
- */
-package hive.properties;
